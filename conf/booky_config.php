ded43624dea8491adfa7e4b24d386ea757144241
windywany <windywany@163.com>
2019-07-31 17:43:58 +0800

    add config

---
+++ conf/booky_config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of wulacms.
+ *
+ * (c) Leo Ning <windywany@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'dir'      => 'doc',
+    'theme'    => 'wula',
+    'theme_en' => 'wen',
+    'langs'    => ['default' => '中文', 'en' => '英文']
+];
